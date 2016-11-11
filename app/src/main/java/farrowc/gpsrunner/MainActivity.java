6f9e9895cb3dc68c6e2690f09f80628ec4af1d88
Leonard <leonardj@msoe.edu>
2016-11-11 08:32:54 -0600

    add permission checks and request starting location if there is none

--- app/src/main/java/farrowc/gpsrunner/MainActivity.java
+++ app/src/main/java/farrowc/gpsrunner/MainActivity.java
@@ -3,18 +3,16 @@ package farrowc.gpsrunner;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
-import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Looper;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -45,6 +43,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
             double longitude = location.getLongitude();
             if (mMap != null) {
                 marker.setPosition(new LatLng(location.getLatitude(),location.getLongitude()));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(defaultPos));
             }
 
 
@@ -97,23 +96,37 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         criteria.setSpeedRequired(false);
         criteria.setCostAllowed(true);
 
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+                String str = "Explanation needed: Please I need to determine your location";
+                Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_READ_LOCATION);
+                String str = "No explanation needed: thanks.";
+                Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
+            }
+        }
+
         //	Obtain	reference	to	location	provider
         List<String> providers = locationManager.getProviders(true);
         bestProvider = providers.get(1);
-        Location l = locationManager.getLastKnownLocation(bestProvider);
-        Location bestLocation = l;
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
+        Log.d("Provider", bestProvider);
+        Location bestLocation = locationManager.getLastKnownLocation(bestProvider);
         locationManager.requestLocationUpdates(bestProvider, (long) 100, 4f, locationListener);
-        defaultPos = new LatLng(bestLocation.getLatitude(), bestLocation.getLongitude());
+        if (bestLocation != null) {
+            defaultPos = new LatLng(bestLocation.getLatitude(), bestLocation.getLongitude());
+            Log.d("Location", "Using previous location");
+        } else {
+            locationManager.requestSingleUpdate(bestProvider, locationListener, Looper.myLooper());
+            Log.d("Location", "Requesting new location");
+        }
     }
 
 
@@ -143,6 +156,7 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         locationManager.requestLocationUpdates(bestProvider, (long) 2000, 10f, locationListener);
     }
 
+    @Override
     public void onPause() {
         super.onPause();
         if (ContextCompat.checkSelfPermission(this,
